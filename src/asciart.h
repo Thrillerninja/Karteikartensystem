060eb8fd15128aacd6e09184be7aece813e1822d
Orbachro <robert.orbach@trumpf.com>
2023-03-08 18:03:51 +0100

    artfunctions in seperate file

---
+++ src/asciart.h
@@ -0,0 +1,12 @@
+//
+// Created by rorba on 08.03.2023.
+//
+
+#ifndef KARTEIKARTENSYSTEM_ASCIART_H
+#define KARTEIKARTENSYSTEM_ASCIART_H
+
+void showMenues(int x);
+void printSolution(char question[], char answer[], int order_number, int max_number, int tries, int max_tries,char fail[]);
+void printQuestion(char question[], char answer[], int order_number, int max_number, int tries, int max_tries);
+
+#endif //KARTEIKARTENSYSTEM_ASCIART_H
